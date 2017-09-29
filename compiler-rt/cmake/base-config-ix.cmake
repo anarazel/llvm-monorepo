anarazel/llvm-monorepo

commit 4df578772be6e5dc2f854619578dc046f37216a0

[CMake] Fix configuration on PowerPC with sanitizers
TEST_BIG_ENDIAN() performs compile tests that will fail with
-nodefaultlibs when building under LLVM_USE_SANITIZER.

Differential Revision: https://reviews.llvm.org/D38277

llvm-svn=314512

compiler-rt/cmake/base-config-ix.cmake
@@ -148,7 +148,13 @@ macro(test_targets)
         endif()
       endif()
     elseif("${COMPILER_RT_DEFAULT_TARGET_ARCH}" MATCHES "powerpc")
+      # Strip out -nodefaultlibs when calling TEST_BIG_ENDIAN. Configuration
+      # will fail with this option when building with a sanitizer.
+      cmake_push_check_state()
+      string(REPLACE "-nodefaultlibs" "" CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
       TEST_BIG_ENDIAN(HOST_IS_BIG_ENDIAN)
+      cmake_pop_check_state()
+
       if(HOST_IS_BIG_ENDIAN)
         test_target_arch(powerpc64 "" "-m64")
       else()

commit 139f6926dc140731a8b4760e589e781932e229dd

Fix cmake file broken by D38277.
llvm-svn=314528

compiler-rt/cmake/base-config-ix.cmake
@@ -151,7 +151,7 @@ macro(test_targets)
       # Strip out -nodefaultlibs when calling TEST_BIG_ENDIAN. Configuration
       # will fail with this option when building with a sanitizer.
       cmake_push_check_state()
-      string(REPLACE "-nodefaultlibs" "" CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
+      string(REPLACE "-nodefaultlibs" "" CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
       TEST_BIG_ENDIAN(HOST_IS_BIG_ENDIAN)
       cmake_pop_check_state()
 